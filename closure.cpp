00d388035b029b3a961e35820c1bddc3a8cc3de6
GenSyhius <sunyuanhao123456@gmail.com>
2015-12-13 00:54:31 +0800

    Closure done

--- closure.cpp
+++ closure.cpp
@@ -24,3 +24,45 @@ MATRIX<double> symmetryClosure(MATRIX<double> temp, int row) {
 	return temp;
 }
 
+MATRIX<double> transitivityClosure(MATRIX<double> temp, int row) {
+	for (int i = 0; i < row; ++i) {
+		for (int j = 0; j < row; ++j) {
+			if(temp[j][i] == 1) {
+				for (int k = 0; k < row; ++k) {
+					temp[j][k] = temp[j][k] + temp[i][k];
+				}
+			} 
+		}
+	} //end for
+
+	for (int x = 0; x < row; ++x) {
+		for (int y = 0; y < row; ++y) {
+			if(temp[x][y] != 0)
+				temp[x][y] = 1;
+		}
+	} //end for
+
+	return temp;
+}
+
+int main(int argc, char const *argv[])
+{
+	int row = 4;
+	MATRIX<double> test(row, row);
+
+	MATRIX<double> test1(row, row);
+
+	test[0][1] = 1;
+	test[2][3] = 1;
+	test1 = transitivityClosure(test, row);
+
+	for (int i = 0; i < row; ++i)
+	{
+		for (int j = 0; j < row; ++j)
+		{
+			cout << test1[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
