a0d5759d9daa29ad7490c3c166e36d6e1245aa9b
GenSyhius <sunyuanhao123456@gmail.com>
2015-12-13 11:53:42 +0800

    change

---
+++ corMatrix.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "Matrix.cpp"
+
+using namespace std;
+
+MATRIX<double> corMatrix(MATRIX<double> temp, int row) {
+	int corraletion;
+	for(i)
+}
+
+1  2  3
+
+0  0  0
+0  1  0
+1  1  0
